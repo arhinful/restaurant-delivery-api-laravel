3341216cd17d3f090d92fc6f7805acb7eb3aca33
Arhinful Emmanuel <kofibusy@gmail.com>
2023-06-22 17:30:02 +0000

    order store

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -7,6 +7,12 @@ use App\Models\Order;
 class OrderService
 {
     public static function store(array $data): Order{
+
+        // check if auth user is an admin and custom user id exists on request data
+        if (auth()->user()->hasRole('admin') && isset($data['custom_user_id'])){
+            // auth user is an admin and this order is being created for normal user
+            $data['user_id'] = $data['custom_user_id'];
+        }
         $order = Order::create($data);
         return $order;
     }
