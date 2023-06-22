7672686c8acc16b3cd4d500aea51e1ece15e7adb
Arhinful Emmanuel <kofibusy@gmail.com>
2023-06-22 08:37:26 +0000

    wip

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
+use App\Http\Resources\UserResource;
 use App\Services\UserService;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\JsonResponse;
@@ -17,7 +18,7 @@ class RegisteredUserController extends Controller
         try {
             $user = UserService::store($request->validated());
             event(new Registered($user));
-            Auth::login($user);
+            $user = UserResource::make($user);
             return $this->successCreated(data: $user, message: "Your account has been created successfully");
         } catch (\Exception $exception){
             return $this->errorOccurred($exception->getMessage());
